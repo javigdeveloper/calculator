45f933633ca418f62a0a3e888085b3f1fd04466f
Javier García <javigdeveloper@gmail.com>
2020-04-12 04:52:41 -0500

    functionality

--- app.js
+++ app.js
@@ -24,6 +24,56 @@ function divideXAndY() {
   document.getElementById("result").innerHTML = x / y;
 }
 
+document.getElementById("mainDisplay").value = "";
+
 function inputNumber(n) {
-  document.getElementById("mainDisplay").value = n;
+  document.getElementById("mainDisplay").value += n.toString();
+}
+
+function add() {
+  document.getElementById("secondaryDisplay").value =
+  document.getElementById("mainDisplay").value + " +";
+  document.getElementById("mainDisplay").value = "";
+}
+function minus() {
+  document.getElementById("secondaryDisplay").value =
+  document.getElementById("mainDisplay").value + " -";
+  document.getElementById("mainDisplay").value = "";
+}
+function times() {
+  document.getElementById("secondaryDisplay").value =
+  document.getElementById("mainDisplay").value + " x";
+  document.getElementById("mainDisplay").value = "";
+}
+function divided() {
+  document.getElementById("secondaryDisplay").value =
+  document.getElementById("mainDisplay").value + " /";
+  document.getElementById("mainDisplay").value = "";
+}
+
+function dot() {
+  document.getElementById("mainDisplay").value += ".";
+}
+
+function equals() {
+  let r = document.getElementById("secondaryDisplay").value;
+  let result = r.split(" ");
+  if(result[1] === "+") {
+    document.getElementById("mainDisplay").value =
+    parseFloat(result[0]) + parseFloat(document.getElementById("mainDisplay").value);
+  } else if(result[1] === "-") {
+    document.getElementById("mainDisplay").value =
+    parseFloat(result[0]) - parseFloat(document.getElementById("mainDisplay").value);
+  } else if(result[1] === "x") {
+    document.getElementById("mainDisplay").value =
+    parseFloat(result[0]) * parseFloat(document.getElementById("mainDisplay").value);
+  } else if(result[1] === "/") {
+    document.getElementById("mainDisplay").value =
+    parseFloat(result[0]) / parseFloat(document.getElementById("mainDisplay").value);
+  }
+}
+
+function clearMain() {
+  document.getElementById("mainDisplay").value = "";
+  document.getElementById("secondaryDisplay").value = "";
 }
