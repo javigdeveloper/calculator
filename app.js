01b3a462fc861103473af5ad300da75e936c5e79
Javier García <javigdeveloper@gmail.com>
2020-04-13 08:33:22 -0500

    digit restriction

--- app.js
+++ app.js
@@ -1,29 +1,154 @@
 let x,y;
+let elem = document.getElementById("result");
 
 function addXAndY(x, y) {
   x = Number(document.getElementById("fieldOne").value);
   y = Number(document.getElementById("fieldTwo").value);
-  document.getElementById("result").innerHTML = x + y;
+  let z = x + y;
+  let arr = z.toString().split(".");
+  let newArr = [];
+  // if result is an integer
+  if(arr.length === 1) {
+    if(arr[0].length > 10) {
+      elem.innerHTML = z.toPrecision(1);
+    } else {
+      elem.innerHTML = z;
+    }
+  // if result is a float
+  } else if(arr.length === 2) {
+    // if decimals are more than 9
+    if(arr[1].length > 9) {
+      elem.innerHTML = z.toPrecision(9);
+    } else {
+      for(let i = 9; i > 0; i--) {
+        let counter = 10 - i;
+        if(arr[0].length > 9) {
+          elem.innerHTML = z.toPrecision(1);
+        } else if(arr[0].length === i) {
+          newArr.push(arr[0]);
+          let decimals = arr[1].slice(0, counter);
+          newArr.push(decimals);
+          z = parseFloat(newArr.join("."));
+          elem.innerHTML = z;
+          break;
+        }
+      }
+    }
+  }
 }
 
 function subtractXAndY() {
   x = Number(document.getElementById("fieldOne").value);
   y = Number(document.getElementById("fieldTwo").value);
-  document.getElementById("result").innerHTML = x - y;
+  let z = x - y;
+  let arr = z.toString().split(".");
+  let newArr = [];
+  // if result is an integer
+  if(arr.length === 1) {
+    if(arr[0].length > 10) {
+      elem.innerHTML = z.toPrecision(1);
+    } else {
+      elem.innerHTML = z;
+    }
+  // if result is a float
+  } else if(arr.length === 2) {
+    // if decimals are more than 9
+    if(arr[1].length > 9) {
+      elem.innerHTML = z.toPrecision(9);
+    } else {
+      for(let i = 9; i > 0; i--) {
+        let counter = 10 - i;
+        if(arr[0].length > 9) {
+          elem.innerHTML = z.toPrecision(1);
+        } else if(arr[0].length === i) {
+          newArr.push(arr[0]);
+          let decimals = arr[1].slice(0, counter);
+          newArr.push(decimals);
+          z = parseFloat(newArr.join("."));
+          elem.innerHTML = z;
+          break;
+        }
+      }
+    }
+  }
 }
 
 function multiplyXAndY() {
   x = Number(document.getElementById("fieldOne").value);
   y = Number(document.getElementById("fieldTwo").value);
-  document.getElementById("result").innerHTML = x * y;
+  let z = x * y;
+  let arr = z.toString().split(".");
+  let newArr = [];
+  // if result is an integer
+  if(arr.length === 1) {
+    if(arr[0].length > 10) {
+      elem.innerHTML = z.toPrecision(1);
+    } else {
+      elem.innerHTML = z;
+    }
+  // if result is a float
+  } else if(arr.length === 2) {
+    // if decimals are more than 9
+    if(arr[1].length > 9) {
+      elem.innerHTML = z.toPrecision(9);
+    } else {
+      for(let i = 9; i > 0; i--) {
+        let counter = 10 - i;
+        if(arr[0].length > 9) {
+          elem.innerHTML = z.toPrecision(1);
+        } else if(arr[0].length === i) {
+          newArr.push(arr[0]);
+          let decimals = arr[1].slice(0, counter);
+          newArr.push(decimals);
+          z = parseFloat(newArr.join("."));
+          elem.innerHTML = z;
+          break;
+        }
+      }
+    }
+  }
 }
+
 
 function divideXAndY() {
   x = Number(document.getElementById("fieldOne").value);
   y = Number(document.getElementById("fieldTwo").value);
-  document.getElementById("result").innerHTML = x / y;
+  let z = x / y;
+  let arr = z.toString().split(".");
+  let newArr = [];
+  // if result is an integer
+  if(arr.length === 1) {
+    if(arr[0].length > 10) {
+      elem.innerHTML = z.toPrecision(1);
+    } else {
+      elem.innerHTML = z;
+    }
+  // if result is a float
+  } else if(arr.length === 2) {
+    // if decimals are more than 9
+    if(arr[1].length > 9) {
+      elem.innerHTML = z.toPrecision(9);
+    } else {
+      for(let i = 9; i > 0; i--) {
+        let counter = 10 - i;
+        if(arr[0].length > 9) {
+          elem.innerHTML = z.toPrecision(1);
+        } else if(arr[0].length === i) {
+          newArr.push(arr[0]);
+          let decimals = arr[1].slice(0, counter);
+          newArr.push(decimals);
+          z = parseFloat(newArr.join("."));
+          elem.innerHTML = z;
+          break;
+        }
+      }
+    }
+  }
 }
 
+
+
+// Second Calculator:
 document.getElementById("mainDisplay").value = "";
 
 function inputNumber(n) {
